0ce46c3b3b4cf1702181156e6748bd25fd522bca
Aserturik <alex.hernandez27@uptc.edu.co>
2025-03-25 10:55:31 -0500

    borro simulacion.py

--- simulacion.py
+++
@@ -1,8 +0,0 @@
-from simulacion.juego import Juego
-
-# Ejecutar la simulación
-if __name__ == "__main__":
-    juego = Juego(
-        "Arqueros del Norte", "Arqueras del Sur", num_jugadores=5, num_rondas=10
-    )
-    juego.jugar_partida_completa()
